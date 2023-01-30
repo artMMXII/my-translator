aca12967d2fe39ccb58ba120e1da78a9497c42ad
Arthur Khasiev <hasiev_artur@mail.ru>
2023-01-30 18:43:41 +0700

    creating simple(primary) interface

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from tkinter import messagebox, Tk, Canvas, Frame, Entry, Button
+from tkinter import *
 
 
 def translator():
@@ -11,7 +12,7 @@ def translator():
                                                                      'jYzY4MTQzMWRiZGU0MmJhYTNhYWEyNmE4'})
 
     param = {
-        'text': 'кот',
+        'text': wordInput.get(),
         'srcLang': 1049,
         'dstLang': 1033,
     }
@@ -21,10 +22,31 @@ def translator():
     }
 
     res = requests.get(api_url, params=param, headers=header)
-
-    print(res.json()['Translation']['Translation'])
-
-    print()
+    info['text'] = res.json()['Translation']['Translation']
 
 
-translator()
+root = Tk()
+
+root['bg'] = 'white'
+root.title('Translator')
+root.wm_attributes('-alpha', 0.9)
+root.geometry('300x250')
+root.resizable(width=False, height=False)
+
+
+frame_top = Frame(root, bg='orange')
+frame_top.place(relx=0.15, rely=0.15, relwidth=0.7, relheight=0.25)
+
+frame_bottom = Frame(root, bg='orange')
+frame_bottom.place(relx=0.15, rely=0.55, relwidth=0.7, relheight=0.1)
+
+wordInput = Entry(frame_top, bg='white')
+wordInput.pack()
+
+btn = Button(frame_top, text='Перевод', bg='white', command=translator)
+btn.pack()
+
+info = Label(frame_bottom, text='Перевод')
+info.pack()
+
+root.mainloop()
